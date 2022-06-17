bb4384887ab71d8779395ef267a4695629847ab0
Stepan_Suprunov <ipsuprunovso@mail.ru>
2022-06-17 15:22:21 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Является ли пятизначное число палиндромом.
+
+Console.WriteLine("Введите пятизначное число");
+string num = Console.ReadLine();
+
+char[] ar = num.ToCharArray();
+
+if (ar.Length == 5)
+{
+
+    if (ar[0] == ar[4])
+    {
+        if (ar[1] == ar[3])
+        {
+            Console.WriteLine("Да");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+else
+{
+    Console.WriteLine("Введено не корректное число");
+}
